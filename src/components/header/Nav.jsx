4a3614711b498e9047336ff4cfa86f86594ec85b
fof002 <80104002+fof002@users.noreply.github.com>
2023-10-25 10:40:53 +0200

    added searchbar

--- src/components/header/Nav.jsx
+++ src/components/header/Nav.jsx
@@ -6,11 +6,11 @@ import Navbar from "react-bootstrap/Navbar";
 
 export function NavVenue() {
   return (
-    <Navbar expand="lg" className="bg-body-secondary">
+    <Navbar expand="lg" className="bg-primary">
       <Container>
         <Navbar.Brand href="#home">
           <img
-            src="/img/Logo.png"
+            src="img/Logo.png"
             width="30"
             height="30"
             className="d-inline-block align-top"
@@ -20,26 +20,32 @@ export function NavVenue() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link className="text-white" href="#home">
+              Home
+            </Nav.Link>
             <ProfileDropdown />
-            <Nav.Link href="#link">About</Nav.Link>
+            <Nav.Link className="text-white" href="#link">
+              About
+            </Nav.Link>
           </Nav>
+          <Form inline>
+            <Row className="d-flex">
+              <Col xs="auto">
+                <Form.Control
+                  type="text"
+                  placeholder="Search our venues..."
+                  className=" mr-sm-2"
+                />
+              </Col>
+              <Col xs="auto">
+                <Button className="border-white" type="submit">
+                  Search
+                </Button>
+              </Col>
+            </Row>
+          </Form>
         </Navbar.Collapse>
       </Container>
-      <Form inline>
-        <Row>
-          <Col xs="auto">
-            <Form.Control
-              type="text"
-              placeholder="Search"
-              className=" mr-sm-2"
-            />
-          </Col>
-          <Col xs="auto">
-            <Button type="submit">Submit</Button>
-          </Col>
-        </Row>
-      </Form>
     </Navbar>
   );
 }
--- src/components/header/ProfileDropdown.jsx
+++ src/components/header/ProfileDropdown.jsx
@@ -2,7 +2,7 @@ import { NavDropdown } from "react-bootstrap";
 
 export function ProfileDropdown() {
   return (
-    <NavDropdown title="Profile" id="basic-nav-dropdown">
+    <NavDropdown className="text-white" title="Profile" id="basic-nav-dropdown">
       <NavDropdown.Item href="#action/3.1">Your bookings</NavDropdown.Item>
       <NavDropdown.Item href="#action/3.2">Change avatar</NavDropdown.Item>
       <NavDropdown.Item href="#action/3.3">Log out</NavDropdown.Item>
