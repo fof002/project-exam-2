a67ba8f664668544b17bd8d614d2976421c2707e
fof002 <80104002+fof002@users.noreply.github.com>
2023-10-29 20:16:42 +0100

    worked on searchinput

--- src/components/header/SearchBar.jsx
+++ src/components/header/SearchBar.jsx
@@ -1,22 +1,22 @@
-import { Form, Row, Col, Button } from "react-bootstrap";
+import { Form, Row, Col, Container } from "react-bootstrap";
 
 export function SearchBar() {
   return (
     <Form inline>
-      <Row className="d-flex">
-        <Col xs="auto">
-          <Form.Control
-            type="text"
-            placeholder="Search our venues..."
-            className=" mr-sm-2 bg-white text-primary"
-          />
-        </Col>
-        <Col xs="auto">
-          <Button className="border-white" type="submit">
-            Search
-          </Button>
-        </Col>
-      </Row>
+      <Container className="bg-white p-1 rounded" id="searchContainer">
+        <Row className="d-flex">
+          <Col xs="auto"></Col>
+          <Col xs="auto">
+            <Form.Control
+              type="text"
+              id="searchInput"
+              placeholder="Search our venues..."
+              className=" mr-sm-2 bg-white .text-black-50 border-0"
+              style={{ minWidth: "20em" }}
+            />
+          </Col>
+        </Row>
+      </Container>
     </Form>
   );
 }
