3fbe3172d4d0bc9e97d748febc8ddf71a076563f
fof002 <80104002+fof002@users.noreply.github.com>
2023-11-01 14:14:13 +0100

    fixed position dorpdown

--- src/components/header/ProfileDropdown.jsx
+++ src/components/header/ProfileDropdown.jsx
@@ -5,6 +5,7 @@ import {
   faUserPlus,
   faUtensils,
   faPersonWalkingLuggage,
+  faCalendarCheck,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
@@ -12,27 +13,37 @@ export function ProfileDropdown() {
   return (
     <NavDropdown title="Profile" id="basic-nav-dropdown">
       <LinkContainer to="/bookings">
-        <NavDropdown.Item className="d-flex align-items-center gap-2" href="#">
+        <NavDropdown.Item className="d-flex align-items-center gap-1" href="#">
           <FontAwesomeIcon icon={faPersonWalkingLuggage} />
           <div>Your bookings</div>
         </NavDropdown.Item>
       </LinkContainer>
-      <NavDropdown.Item className="d-flex align-items-center gap-2" href="#">
+      <NavDropdown.Item className="d-flex align-items-center gap-1" href="#">
+        <FontAwesomeIcon icon={faCalendarCheck} />
+        <div>Book a venue</div>
+      </NavDropdown.Item>
+      <NavDropdown.Item className="d-flex align-items-center gap-1" href="#">
         <FontAwesomeIcon icon={faUserPlus} /> <div>Change avatar</div>
       </NavDropdown.Item>
       <NavDropdown.Divider />
       <LinkContainer to="/venues">
         <NavDropdown.Item
-          className="d-flex align-items-center gap-2"
+          className="d-flex align-items-center gap-1"
           href="/venues"
         >
-          <FontAwesomeIcon icon={faUtensils} />
+          <FontAwesomeIcon
+            icon={faUtensils}
+            style={{ position: "relative", top: "0.15em" }}
+          />
           <div>Your venues</div>
         </NavDropdown.Item>
       </LinkContainer>
       <NavDropdown.Divider />
-      <NavDropdown.Item className="d-flex align-items-center gap-2" href="#">
-        <FontAwesomeIcon icon={faArrowRightFromBracket} />
+      <NavDropdown.Item className="d-flex align-items-center gap-1" href="#">
+        <FontAwesomeIcon
+          icon={faArrowRightFromBracket}
+          style={{ position: "relative", top: "0.15em" }}
+        />
         <div>Log out</div>
       </NavDropdown.Item>
     </NavDropdown>
