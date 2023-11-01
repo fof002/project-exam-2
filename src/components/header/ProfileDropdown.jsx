2f34f7b4fc5d107afaf6cc3e200feba3ce31a969
fof002 <80104002+fof002@users.noreply.github.com>
2023-11-01 12:31:12 +0100

    added linkcontainer

--- src/components/header/ProfileDropdown.jsx
+++ src/components/header/ProfileDropdown.jsx
@@ -1,13 +1,17 @@
 import { NavDropdown } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { LinkContainer } from "react-router-bootstrap";
 
 export function ProfileDropdown() {
   return (
     <NavDropdown title="Profile" id="basic-nav-dropdown">
-      <Link to="/bookings">Your bookings</Link>
+      <LinkContainer to="/bookings">
+        <NavDropdown.Item href="#">Your bookings</NavDropdown.Item>
+      </LinkContainer>
       <NavDropdown.Item href="#">Change avatar</NavDropdown.Item>
       <NavDropdown.Divider />
-      <NavDropdown.Item href="/venues">Your venues</NavDropdown.Item>
+      <LinkContainer to="/venues">
+        <NavDropdown.Item href="/venues">Your venues</NavDropdown.Item>
+      </LinkContainer>
       <NavDropdown.Divider />
       <NavDropdown.Item href="#">Log out</NavDropdown.Item>
     </NavDropdown>
