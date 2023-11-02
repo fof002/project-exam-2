1db4f0a0f5968fc6fca3f1de1c57a04798a1eb93
fof002 <80104002+fof002@users.noreply.github.com>
2023-11-02 14:25:16 +0100

    fixed footer

--- src/components/footer/Index.jsx
+++ src/components/footer/Index.jsx
@@ -2,34 +2,30 @@ import { Link } from "react-router-dom";
 
 export function FooterComponent() {
   return (
-    <footer className="py-3 my-4">
+    <footer className="py-3 bg-primary text-white">
       <ul className="nav justify-content-center border-bottom pb-3 mb-3">
         <li className="nav-item">
           <Link className="text-decoration-none" to="/">
-            <div className="nav-link px-2 text-body-secondary">Home</div>
+            <div className="nav-link px-2 text-white">Home</div>
           </Link>
         </li>
         <li className="nav-item">
           <Link className="text-decoration-none" to="/about">
-            <div className="nav-link px-2 text-body-secondary">About</div>
+            <div className="nav-link px-2 text-white">About</div>
           </Link>
         </li>
         <li className="nav-item">
           <Link className="text-decoration-none" to="/venues">
-            <div className="nav-link px-2 text-body-secondary">Venues</div>
+            <div className="nav-link px-2 text-white">Venues</div>
           </Link>
         </li>
         <li className="nav-item">
           <Link className="text-decoration-none" to="/bookings">
-            <div className="nav-link px-2 text-body-secondary">
-              Your bookings
-            </div>
+            <div className="nav-link px-2 text-white">Your bookings</div>
           </Link>
         </li>
       </ul>
-      <p className="text-center text-body-secondary">
-        © 2023 Holiday - Dream Venues
-      </p>
+      <p className="text-center ">© 2023 Holiday - Dream Venues</p>
     </footer>
   );
 }
