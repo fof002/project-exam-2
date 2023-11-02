b88c40015bd800e6b992a758881b277848d9139c
fof002 <80104002+fof002@users.noreply.github.com>
2023-11-02 12:45:22 +0100

    added form

--- src/components/layout/Index.jsx
+++ src/components/layout/Index.jsx
@@ -5,10 +5,18 @@ import { Outlet } from "react-router-dom";
 export function Layout() {
   return (
     <div id="fullContainer">
-      <HeaderComponent />
-      <main id="mainContainer">
-        <Outlet />
-      </main>
+      <div>
+        <HeaderComponent />
+        <div className="d-flex flex-column align-items-center mt-4 ms-3 me-3">
+          <main
+            id="mainContainer"
+            className="d-flex flex-column align-items-center"
+            style={{ width: "min(60em,100%)" }}
+          >
+            <Outlet />
+          </main>
+        </div>
+      </div>
       <FooterComponent />
     </div>
   );
