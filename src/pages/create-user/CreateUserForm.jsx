7de0f26c7e5fb2b6c45514ca232da1b2a4e52f82
fof002 <80104002+fof002@users.noreply.github.com>
2023-11-02 14:20:02 +0100

    moved redirect

--- src/pages/create-user/CreateUserForm.jsx
+++ src/pages/create-user/CreateUserForm.jsx
@@ -4,37 +4,42 @@ import { createUser } from "./createUser";
 
 export function CreateUserForm() {
   return (
-    <Form style={{ width: "min(40em,100%)" }}>
-      <Form.Group className="mb-3" controlId="Username">
-        <Form.Label>Username</Form.Label>
-        <Form.Control type="text" />
-        <Form.Text className="text-muted"></Form.Text>
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="Email">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" />
-        <Form.Text className="text-muted">
-          Must be a valid @noroff.no or @stud.noroff.no email
-        </Form.Text>
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="Password">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" />
-        <Form.Text className="text-muted">
-          Must be at least 8 characters long
-        </Form.Text>
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="Avatar">
-        <Form.Label>Profile image</Form.Label>
-        <Form.Control type="text" />
-        <Form.Text className="text-muted">Must be a valid image URL</Form.Text>
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="Checkbox">
-        <Form.Check type="checkbox" label="Register as a venue manager" />
-      </Form.Group>
-      <Button onClick={createUser} variant="primary" type="submit">
-        Create User
-      </Button>
-    </Form>
+    <div style={{ width: "min(40em,100%)" }}>
+      <h1>Create new user</h1>
+      <Form>
+        <Form.Group className="mb-3" controlId="Username">
+          <Form.Label>Username</Form.Label>
+          <Form.Control type="text" />
+          <Form.Text className="text-muted"></Form.Text>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="Email">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" />
+          <Form.Text className="text-muted">
+            Must be a valid @noroff.no or @stud.noroff.no email
+          </Form.Text>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="Password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" />
+          <Form.Text className="text-muted">
+            Must be at least 8 characters long
+          </Form.Text>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="Avatar">
+          <Form.Label>Profile image</Form.Label>
+          <Form.Control type="text" />
+          <Form.Text className="text-muted">
+            Must be a valid image URL
+          </Form.Text>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="Checkbox">
+          <Form.Check type="checkbox" label="Register as a venue manager" />
+        </Form.Group>
+        <Button onClick={createUser} variant="primary" type="submit">
+          Create User
+        </Button>
+      </Form>
+    </div>
   );
 }
--- src/pages/login/LoginForm.jsx
+++ src/pages/login/LoginForm.jsx
@@ -5,24 +5,27 @@ import { login } from "./login";
 
 export function LoginForm() {
   return (
-    <Form style={{ width: "min(40em,100%)" }}>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted"></Form.Text>
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Button onClick={login} variant="primary" type="submit">
-        Login
-      </Button>
-      <Link to="/create-user">
-        <Button className="ms-2" variant="primary" type="submit">
-          Create User
+    <div style={{ width: "min(40em,100%)" }}>
+      <h1>Login</h1>
+      <Form style={{ width: "min(40em,100%)" }}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted"></Form.Text>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Button onClick={login} variant="primary" type="submit">
+          Login
         </Button>
-      </Link>
-    </Form>
+        <Link to="/create-user">
+          <Button className="ms-2" variant="primary" type="submit">
+            Create User
+          </Button>
+        </Link>
+      </Form>
+    </div>
   );
 }
