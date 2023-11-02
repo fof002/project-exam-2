11b8bbbdb579fa1d422d1f4e3437bd4ed83d4fd0
fof002 <80104002+fof002@users.noreply.github.com>
2023-11-02 13:55:22 +0100

    removed placeholders

--- src/pages/create-user/CreateUserForm.jsx
+++ src/pages/create-user/CreateUserForm.jsx
@@ -7,26 +7,26 @@ export function CreateUserForm() {
     <Form style={{ width: "min(40em,100%)" }}>
       <Form.Group className="mb-3" controlId="Username">
         <Form.Label>Username</Form.Label>
-        <Form.Control type="text" placeholder="Enter username" />
+        <Form.Control type="text" />
         <Form.Text className="text-muted"></Form.Text>
       </Form.Group>
       <Form.Group className="mb-3" controlId="Email">
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Control type="email" />
         <Form.Text className="text-muted">
           Must be a valid @noroff.no or @stud.noroff.no email
         </Form.Text>
       </Form.Group>
       <Form.Group className="mb-3" controlId="Password">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control type="password" />
         <Form.Text className="text-muted">
           Must be at least 8 characters long
         </Form.Text>
       </Form.Group>
       <Form.Group className="mb-3" controlId="Avatar">
         <Form.Label>Profile image</Form.Label>
-        <Form.Control type="text" placeholder="Image URL" />
+        <Form.Control type="text" />
         <Form.Text className="text-muted">Must be a valid image URL</Form.Text>
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicCheckbox">
