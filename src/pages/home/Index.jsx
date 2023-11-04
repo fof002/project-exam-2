6360ac81fa763a89870870cb500fbf08c7f83ab4
fof002 <80104002+fof002@users.noreply.github.com>
2023-11-04 11:38:45 +0100

    fixed width issue

--- src/pages/home/Index.jsx
+++ src/pages/home/Index.jsx
@@ -2,7 +2,7 @@ import { GetVenues } from "../../components/api/get/GetVenues";
 
 export function HomePage() {
   return (
-    <div>
+    <div style={{ width: "100%" }}>
       <h1 className="border-bottom border-primary pb-3 mb-3 text-center">
         Holidaze - Dream Venues
       </h1>
