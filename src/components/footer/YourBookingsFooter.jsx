27ad81d1d9977a81761c6ae674eeef4caedefd43
fof002 <80104002+fof002@users.noreply.github.com>
2023-12-02 08:09:46 +0100

    added bookings link in footer

--- src/components/footer/YourBookingsFooter.jsx
+++ src/components/footer/YourBookingsFooter.jsx
@@ -1,11 +1,14 @@
 import { Link } from "react-router-dom";
+import { LinkContainer } from "react-router-bootstrap";
 
 export function YourBookingsFooter() {
   return localStorage.getItem("user") !== null ? (
     <li className="nav-item">
-      <Link className="text-decoration-none" to="/bookings">
-        <div className="nav-link px-2 text-white">Your bookings</div>
-      </Link>
+      <LinkContainer to="/bookings">
+        <Link className="text-decoration-none" to="/bookings">
+          <div className="nav-link px-2 text-white">Your bookings</div>
+        </Link>
+      </LinkContainer>
     </li>
   ) : (
     ""
